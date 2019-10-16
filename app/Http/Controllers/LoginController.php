266d6bb3f0caa9610df4745e13f8ddb14dfe3d95
楊承昊 <jerry@yangchenghaode-MacBook-Pro.local>
2019-10-16 10:54:06 +0800

    Add controller and view init

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function show()
+    {
+        return view('login');
+    }
+
+    public function login()
+    {
+        # code...
+    }
+
+    public function logout()
+    {
+        # code...
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    {{-- CSRF Token --}}
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@if (trim($__env->yieldContent('template_title')))@yield('template_title') | @endif {{ config('app.name', Lang::get('titles.app')) }}</title>
+    <meta name="description" content="">
+    <meta name="author" content="Jerry Yang">
+
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+
+</head>
+<body>
+    <div id="app">
+
+        {{-- @include('partials.nav') --}}
+
+        <main class="py-4">
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        @include('partials.form-status')
+                    </div>
+                </div>
+            </div>
+
+            @yield('content')
+
+        </main>
+
+    </div>
+
+    {{-- Scripts --}}
+    <script src="{{ asset('/js/app.js') }}"></script>
+
+    @yield('footer_scripts')
+
+</body>
+</html>
