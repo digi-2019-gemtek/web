14ab1c76ef0100fa7095f085c4e4f93e2b394bda
tico88612 <tico88612@yahoo.com.tw>
2019-10-27 23:16:23 +0800

    Add coreui remove sb-admin-2

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,11 @@
  */
 
 require('./bootstrap');
-require('../../node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.min.js')
+// require('../../node_modules/startbootstrap-sb-admin-2/js/sb-admin-2.min.js')
+require('../../node_modules/jquery/dist/jquery.min.js')
+require('../../node_modules/popper.js/dist/umd/popper.min.js')
+require('../../node_modules/bootstrap/dist/js/bootstrap.min.js')
+require('../../node_modules/@coreui/coreui/dist/js/coreui.min.js')
 
 // window.Vue = require('vue');
 
